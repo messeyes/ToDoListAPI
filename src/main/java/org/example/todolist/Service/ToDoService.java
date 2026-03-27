a706ac54a0978aee3a2bd4d147e5f1875d149cd3
mess <2006mesousasilva@gmail.com>
2026-03-27 16:02:34 -0300

    feat: Create ToDo service

--- src/main/java/org/example/todolist/Service/ToDoService.java
+++ src/main/java/org/example/todolist/Service/ToDoService.java
@@ -1,4 +1,40 @@
 package org.example.todolist.Service;
 
+import org.example.todolist.Model.ToDo;
+import org.example.todolist.Repository.ToDoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ToDoService {
+    private ToDoRepository toDoRepository;
+
+    public void setToDoRepository(ToDoRepository toDoRepository) {
+        this.toDoRepository = toDoRepository;
+    }
+
+
+    public List<ToDo> create(ToDo toDo){
+        toDoRepository.save(toDo);
+        return list();
+    }
+
+    public List<ToDo> list(){
+        Sort sort = Sort.by("priority").descending().and(
+                Sort.by("title").ascending()
+        );
+       return toDoRepository.findAll(sort);
+    }
+
+    public List<ToDo> update(ToDo todo){
+        toDoRepository.save(todo);
+        return list();
+    }
+
+    public List<ToDo> delete(Long id){
+        toDoRepository.deleteById(id);
+        return list();
+    }
 }
