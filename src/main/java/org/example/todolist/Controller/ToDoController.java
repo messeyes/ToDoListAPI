c06e64a1ada8f05b7a39fe72c2e53fe2dcddcfec
mess <2006mesousasilva@gmail.com>
2026-03-27 16:13:02 -0300

    feat: Create ToDo controller

--- src/main/java/org/example/todolist/Controller/ToDoController.java
+++ src/main/java/org/example/todolist/Controller/ToDoController.java
@@ -1,4 +1,40 @@
 package org.example.todolist.Controller;
 
+
+import org.example.todolist.Model.ToDo;
+import org.example.todolist.Service.ToDoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+
 public class ToDoController {
+    private ToDoService toDoService;
+
+    public void ToDoController(ToDoService toDoService){
+        this.toDoService = toDoService;
+    }
+
+    @PostMapping
+    List<ToDo> create(@RequestBody ToDo toDo){
+        return toDoService.create(toDo);
+    }
+
+    @GetMapping
+    List<ToDo> list(){
+        return toDoService.list();
+    }
+
+    @PutMapping
+    List<ToDo> update(@RequestBody ToDo toDo){
+        return toDoService.update(toDo);
+    }
+
+    @DeleteMapping("{id")
+    List<ToDo> delete(@PathVariable("id") Long id){
+        return toDoService.delete(id);
+    }
+
 }
