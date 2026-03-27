3be67df1b92f78f365f0b15cfd055d6bfcb268f7
mess <2006mesousasilva@gmail.com>
2026-03-27 15:33:34 -0300

    feat: Create ToDo entity

--- src/main/java/org/example/todolist/Model/ToDo.java
+++ src/main/java/org/example/todolist/Model/ToDo.java
@@ -1,4 +1,55 @@
 package org.example.todolist.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
 public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Boolean completed;
+    private int priority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
